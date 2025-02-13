6f01e438418f8c943f95e179e8a4ba486774e94e
Dmitry <demaster.easy@gmail.com>
2025-02-13 21:34:00 +0300

    Вторая секция с навыками начата

---
+++ my-portfolio-site-base/scripts/index.js
@@ -0,0 +1,14 @@
+const buttons = document.querySelectorAll(".card_button");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+        const card = button.closest(".skill_card");
+
+        const description = card.querySelector(".card_decription");
+        const arrow = card.querySelector(".card_button_arrow");
+
+		card.classList.toggle("unwrap")
+        description.classList.toggle("unwrap");
+        arrow.classList.toggle("unwrap");
+    });
+});
